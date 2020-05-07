7ecfb6048da4e9a17bed6f47bc674bef770772ac
Telmo Menezes <telmo@telmomenezes.net>
2020-05-07 16:12:46 +0200

    script to split large file

--- split_file.py
+++ split_file.py
@@ -5,11 +5,17 @@ import gzip
 
 if __name__ == '__main__':
     with open('reddit-submissions.json', 'r') as f:
+        last_month_year = None
+        of = None
         for line in f:
             data = json.loads(line)
             ts = data['created_utc']
             month_year = datetime.utcfromtimestamp(ts).strftime('%Y-%m')
-            print(month_year)
-            outfile = 'reddit-submissions/{}.json.gz'.format(month_year)
-            with gzip.open(outfile, 'at') as of:
-                of.write('{}\n'.format(json.dumps(data)))
+            if last_month_year != month_year:
+                if of:
+                    of.close()
+                outfile = 'reddit-submissions/{}.json.gz'.format(month_year)
+                of = gzip.open(outfile, 'at')
+                last_month_year = month_year
+            of.write('{}\n'.format(json.dumps(data)))
+        of.close()
